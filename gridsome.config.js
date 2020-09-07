7d6dd1cba69189e6ab80d6b0fb5fc688fae0b5c7
Steve_Zhang <wentaozhang0305@gmail.com>
2020-09-07 15:50:06 -0700

    set up vuetify

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: 'Eventer',
+  siteDescription: 'Post or find your events',
+  plugins: [],
+};
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout">
+  <v-app>
     <header class="header">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
@@ -9,42 +9,20 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
-  </div>
+    <p>{{ $static.metadata.siteDescription }}</p>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
 </template>
 
 <static-query>
 query {
   metadata {
-    siteName
+    siteName,
+    siteDescription
   }
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
+<style></style>
